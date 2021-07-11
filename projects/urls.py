67325ad6492aa7134943d10849bfe00d0ecd4df7
kayongo brian <kayonbrian@gmail.com>
2021-07-12 02:52:06 +0300

    added the model form for adding in new projects

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('', Index, name='index'),
     path('project/<str:pk>/', project, name='project'),
+    path('create-project/', Createproject, name='create-project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .forms import ProjectForm
 
 
 def Index(request):
     projects = Project.objects.all()
-    print(projects)
     context = {'projects': projects}
     return render(request, 'projects.html', context)
 
@@ -14,3 +14,9 @@ def project(request, pk):
     project = Project.objects.get(id=pk)
     context = {'project': project}
     return render(request, 'single-project.html', context)
+
+
+def Createproject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projectform.html', context)
