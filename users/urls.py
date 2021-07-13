f81a85b9e890a27edab8088a8430fd6c5ddf1ce1
kayongo brian <kayonbrian@gmail.com>
2021-07-13 05:00:32 +0300

    added the different pages on the site

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('profile/<str:pk>', views.userProfile, name='user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,3 +6,12 @@ def profile(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topskills = profile.skill_set.exclude(description__exact=" ")
+    otherskills = profile.skill_set.filter(description__exact="")
+    context = {'profile': profile, 'topskills': topskills,
+               'otherskills': otherskills}
+    return render(request, 'users/user-profile.html', context)
