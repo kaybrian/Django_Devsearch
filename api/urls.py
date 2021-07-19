6c98a54f85d47d04d470e8cd95fb97e51571e0d2
kayongo brian <kayonbrian@gmail.com>
2021-07-19 08:43:31 +0300

    working serializer with permissions

--- api/urls.py
+++ api/urls.py
@@ -8,14 +8,14 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('users/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
 
     path('', views.getRoutes),
     path('projects/', views.getProjects),
     path('project/<str:pk>', views.getProject),
-
-
+    path('project/<str:pk>/vote/', views.projectVote),
 
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -27,8 +27,25 @@ def getProjects(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def getProject(request, pk):
     projects = Project.objects.get(id=pk)
     serializer = ProjectSerializer(projects, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project
+    )
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
+    serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
